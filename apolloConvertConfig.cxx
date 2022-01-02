4d4a4b033cb7a05740ed3c295a74d41116b445db
Maik Froechtenicht <maik.froechtenicht@gmail.com>
2022-01-02 14:22:50 +0100

    upate function to const

--- apolloConvertConfig.cxx
+++ apolloConvertConfig.cxx
@@ -13,31 +13,31 @@ void ApolloConvertConfig::setFileName(const std::string& file_name) {
   file_name_m = file_name;
 }
 
-std::string ApolloConvertConfig::getFileName() { return file_name_m; }
+std::string ApolloConvertConfig::getFileName() const { return file_name_m; }
 
 void ApolloConvertConfig::setPressureUnit(const std::string& pre_unit) {
   pre_unit_m = pre_unit;
 }
 
-std::string ApolloConvertConfig::getPressureUnit() { return pre_unit_m; }
+std::string ApolloConvertConfig::getPressureUnit() const { return pre_unit_m; }
 
 void ApolloConvertConfig::setKeepImpulse(bool keep_impulse) {
   keep_impulse_m = keep_impulse;
 }
 
-bool ApolloConvertConfig::getKeepImpulse() { return keep_impulse_m; }
+bool ApolloConvertConfig::getKeepImpulse() const { return keep_impulse_m; }
 
 void ApolloConvertConfig::setCompensate(bool compensate) {
   compensate_m = compensate;
 }
 
-bool ApolloConvertConfig::getCompensate() { return compensate_m; }
+bool ApolloConvertConfig::getCompensate() const { return compensate_m; }
 
 void ApolloConvertConfig::setFileVersion(const std::string& file_version) {
   file_version_m = file_version;
 }
 
-std::string ApolloConvertConfig::getFileVersion() { return file_version_m; }
+std::string ApolloConvertConfig::getFileVersion() const { return file_version_m; }
 
 bool ApolloConvertConfig::isValidFile() {
   if (file_name_m.length() == 0) {
